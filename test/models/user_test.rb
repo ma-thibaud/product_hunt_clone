5f954bc4ec48d6e50802f9161e1856d7faafcc7e
Mathias Thibaud <math.thibaud@gmail.com>
2020-03-10 14:49:06 -0700

    Added a few tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = users(:george)
+    assert_equal "George Abitbol", user.full_name
+  end
 end
